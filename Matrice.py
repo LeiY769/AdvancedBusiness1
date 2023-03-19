d1c071334dc78aa1d8d8d1f12a26946318ea4656
LeiY769 <79572071+LeiY769@users.noreply.github.com>
2023-03-19 16:22:37 +0100

    Add files via upload

---
+++ Matrice.py
@@ -0,0 +1,111 @@
+import numpy as np
+import time
+import scipy.sparse as sp
+
+def createMatrix(value):
+    A = np.zeros((value,value))
+    size = value
+    for i in range(2* value):
+        row = np.random.randint(0,size)
+        column = np.random.randint(0,size)
+        value = np.random.randint(0,10)
+        if row > 0:
+            row = row - 1
+        if column > 0:
+            column = column - 1
+
+        A[row][column] = value
+    A_sparse = sp.csc_matrix(A)
+
+    return A_sparse
+
+def MultiplyMatrix(A,B,value):
+    C = np.zeros((value, value), dtype=np.uint32)
+    for i in range(value):
+        for j in range(value):
+            for k in range(value):
+                C[i][j] += A[i][k] * B[k][j]
+ 
+    return C
+def MultiplySparseMatrix(A,B,value):
+    C_p = np.zeros(value + 1, dtype=np.uint32)
+    C_p[0] = 0
+    nz = 0
+    count2 = 0
+    z = 0
+    w = np.zeros(value,dtype=np.uint32)
+    fastfree = np.zeros(value,dtype=np.uint32)
+    x = np.zeros(value,dtype = np.uint32)
+    for i in range(value):
+        j = B.indptr[i]
+        while j < B.indptr[i+1]:
+            line = B.indices[j]
+            k = A.indptr[line]
+            while k < A.indptr[line+1]:
+                z = A.indices[k]
+                if(w[z] == 0):
+                    w[z] = 1
+                    fastfree[count2] = z
+                    count2 += 1
+                    nz += 1
+                k +=1
+            j +=1
+        C_p[i+1] = C_p[i]+ count2
+        for ii in range(count2):
+            tempo = fastfree[ii]
+            w[tempo] = 0
+            fastfree[ii]= 0
+        count2 = 0
+    #Correct pour sur 
+    count2 = 0
+    count = 0
+    C_indices = np.zeros(nz,dtype=np.uint32)
+    C_data = np.zeros(nz,dtype=np.uint32)
+    for i in range(value):
+        j = B.indptr[i]
+        while j < B.indptr[i+1]:
+            value = B.data[j]
+            line = B.indices[j]
+            k = A.indptr[line]
+            while k < A.indptr[line+1]:
+                z = A.indices[k]
+                if(x[z] == 0):
+                    fastfree[count2] = z
+                    count2 += 1
+                value2 = A.data[k]
+                x[z] += value * value2
+                k += 1
+            j +=1
+        for ii in range(count2):
+            tempo = fastfree[ii]
+            C_indices[count] = tempo 
+            C_data[count] = x[tempo] 
+            count += 1
+            x[tempo] = 0
+            fastfree[ii] = 0
+        count2 = 0
+
+    return C_p,C_indices,C_data
+def MapReduce(A,B):
+
+    return C
+
+start_time = time.time()
+A = createMatrix(100000)
+B = createMatrix(100000)
+#A.data
+#A.indices
+#A.indptr 
+print("--- %s seconds ---" % (time.time() - start_time))
+start_time = time.time()
+C = np.dot(A,B)
+
+print("--- %s seconds ---" % (time.time() - start_time))
+start_time = time.time()
+C1,C2,C3 = MultiplySparseMatrix(A,B,100000)
+
+
+print("--- %s seconds ---" % (time.time() - start_time))
+
+
+
